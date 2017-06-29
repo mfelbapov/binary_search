f499f4033412af0dad0299afcb1dc13ffa120ed7
Milorad Felbapov <miloradfelbapov@Milorads-MacBook-Pro.local>
2017-06-29 15:04:37 -0400

    Final commit

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -6,9 +6,45 @@ describe("Binary Search Algorithm", () => {
     expect(true).toBe(true);
   });
 
-  it("Should throw an error if array is empty", () => {
-    expect(() => binary([])).toThrow(new Error("Array needs to have at least element"))
+  it("Should return -1 if array is empty", () => {
+    expect(binary([], 6)).toEqual(-1)
+  });
+
+  it("Should return an index in array of size one, of number we are looking for", () => {
+    expect(binary([1], 1)).toEqual(0);
+  });
+
+  it("Should return an index of -1 if the element does not exist", () => {
+    expect(binary([1], 6)).toEqual(-1);
+  });
+
+  it("Should return an index in array of size two, of number we are looking for", () => {
+    expect(binary([4, 5], 4)).toEqual(0);
   })
 
-  
+  it("Should return an index in array of size two, of number we are looking for", () => {
+    expect(binary([6, 7], 7)).toEqual(1);
+  })
+
+  it("Should return an index in array of odd size 5, and number in on pivot", () => {
+    expect(binary([1, 2, 4, 6, 7], 4)).toEqual(2)
+  })
+
+  it("Should return an index in array of odd size 5, and number in lower half", () => {
+    expect(binary([1, 2, 4, 6, 7], 2)).toEqual(1);
+  })
+
+  it("Should return an index in array of odd size 5, and number in upper half", () => {
+    expect(binary([1, 2, 4, 6, 7], 6)).toEqual(3);
+  })
+
+  it("Should return an index in array of even size 6 , and nuber is in lower half", () => {
+    expect(binary([1, 2, 4, 6, 7, 8], 4)).toEqual(2);
+  })
+
+  it("Should return an index in array of any size, of number we are looking for", () => {
+    expect(binary([1, 2, 4, 6, 7, 8], 7)).toEqual(4);
+  })
+
+
 })
--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
-export const binary = (arr) => {
-  if(arr.length < 1){
-    throw new Error("Array needs to have at least element");
+
+export const binary = (arr, num) => {
+
+  if(arr.length < 1 || (arr.length == 1 && arr[0] != num)){
+    return -1;
+  } else if(arr.length == 1 && arr[0] == num){
+    return 0;
+  }
+
+  let pivot = Math.floor(arr.length/2);
+  let first = 0;
+  let last = arr.length - 1;
+
+  if(arr[pivot] == num) {
+    return pivot;
+
+  } else if(arr[pivot] > num) {
+    last = pivot;
+    let array = arr.slice(0, last);
+    return binary(array, num);
+
+  } else {
+    first = pivot;
+    let array2 = arr.slice(first, last + 1);
+    return binary(array2, num) + pivot;
   }
 }
